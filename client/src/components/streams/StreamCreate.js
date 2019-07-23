cff09d325966837a9479bc13b2011ed9fa94c113
Zeyang Yi <danielzeyangyi@gmail.com>
2019-07-22 20:17:53 -0400

    builk action creators

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends Component {
   renderError({ error, touched }) {
@@ -23,9 +25,9 @@ class StreamCreate extends Component {
     );
   };
 
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -60,7 +62,15 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+// to avoid nasty sytax
+const formWrapped = reduxForm({
   form: 'createStream', // name of the form,
   validate
 })(StreamCreate);
+
+export default connect(
+  null,
+  {
+    createStream
+  }
+)(formWrapped);
