ed7bd45fe1e56bdad7abd925dc7e5ea1d04f12ff
Zeyang Yi <danielzeyangyi@gmail.com>
2019-07-23 22:13:03 -0400

    finish app base

--- client/src/components/streams/SreamShow.js
+++ client/src/components/streams/SreamShow.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+class StreamShow extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    const { stream } = this.props;
+
+    return !stream ? (
+      <div>Loading....</div>
+    ) : (
+      <div>
+        <h1>{stream.title}</h1>
+        <h5>{stream.description}</h5>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
 };
 
-export default StreamShow;
+export default connect(
+  mapStateToProps,
+  {
+    fetchStream
+  }
+)(StreamShow);
